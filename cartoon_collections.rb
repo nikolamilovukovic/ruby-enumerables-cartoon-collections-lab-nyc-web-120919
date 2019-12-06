257ac33124f85b309f05a694569065e248785071
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-12-06 02:05:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,8 +21,6 @@ def long_planeteer_calls(array)
   end 
 end
 
-long_planeteer_calls(call_screams)
-
 def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
